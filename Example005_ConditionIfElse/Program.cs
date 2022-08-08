57e4d8385c55a70c9284764933b83353295aa64e
ElmiraFaizova <patent_k@mail.ru>
2022-08-08 09:52:52 +0400

    Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "эльмира")
+{
+    Console.WriteLine("Ура, это же Эльмира!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine (username);
+}
+  
